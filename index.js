d9d35c9773883ce2873183ddc8622a9f8dcd41bb
skyrippa <iagob4599@gmail.com>
2020-01-24 20:02:00 -0300

    enable cors

--- index.js
+++ index.js
@@ -27,6 +27,16 @@ if (!db) {
 	console.log('Db connected successfully')
 }
 
+// enabling CORS
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 // Setup server port
 var port = process.env.PORT || 8080;
 
